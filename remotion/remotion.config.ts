4e7bc9a1803a681fe74a67f95b6cb20677138245
Bavos <87132712+Bavos@users.noreply.github.com>
2026-05-06 19:09:59 -0300

    Delete remotion directory

--- remotion/remotion.config.ts
+++
@@ -1,6 +0,0 @@
-import {Config} from '@remotion/cli/config';
-
-Config.setVideoImageFormat('jpeg');
-Config.setOverwriteOutput(true);
-Config.setBrowserExecutable('/usr/bin/google-chrome-stable');
-Config.setChromiumOpenGlRenderer('angle');
