9f4fa53a43e08642c4d288cecf37251dd6d12a79
Bavos <87132712+Bavos@users.noreply.github.com>
2026-05-06 22:51:07 -0300

    Delete meuprontuario-remotion directory

--- meuprontuario-remotion/remotion.config.ts
+++
@@ -1,6 +0,0 @@
-import {Config} from '@remotion/cli/config';
-
-Config.setOverwriteOutput(true);
-Config.setMuted(true);
-Config.setVideoImageFormat('jpeg');
-Config.setChromiumOpenGlRenderer('angle');
